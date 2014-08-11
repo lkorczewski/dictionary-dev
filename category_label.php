19312dea4b880f43e114928a8410aaf9b9bbc166
Łukasz Korczewski <lukasz.korczewski@gmail.com>
2014-08-11 20:31:20 +0000

    category label extending value

--- category_label.php
+++ category_label.php
@@ -2,47 +2,9 @@
 
 namespace Dictionary;
 
-require_once __DIR__ . '/dictionary.php';
+require_once __DIR__ . '/value.php';
 
-class Category_Label {
-	private $dictionary;
-	
-	private $id;
-	private $label;
-	
-	//------------------------------------------------
-	// constructor
-	//------------------------------------------------
-	
-	function __construct(Dictionary $dictionary, $label = NULL){
-		$this->dictionary = $dictionary;
-		
-		if($label) $this->label = $label;
-	}
-	
-	//------------------------------------------------
-	// id management
-	//------------------------------------------------
-	
-	function set_id($id){
-		$this->id = $id;
-	}
-	
-	function get_id(){
-		return $this->id;
-	}
-	
-	//------------------------------------------------
-	// setting value
-	//------------------------------------------------
-	
-	function set($label){
-		$this->label = $label;
-	}
-	
-	function get(){
-		return $this->label;
-	}
+class Category_Label extends Value {
 	
 }
 
