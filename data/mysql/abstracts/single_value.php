28528db0b2b5f0d7267f97b641094f568f436bbc
Łukasz Korczewski <lukasz.korczewski@gmail.com>
2014-10-08 18:14:56 +0200

    abstracting single value mapper

---
+++ data/mysql/abstracts/single_value.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Dictionary;
+
+require_once __DIR__ . '/mapper.php';
+
+abstract class MySQL_Single_Value extends MySQL_Mapper {
+	
+	protected $table_name;
+	protected $element_name;
+	
+	//------------------------------------------------------------------
+	// creating context storage (table)
+	//------------------------------------------------------------------
+	
+	function create_storage(){
+		$query =
+			"CREATE TABLE IF NOT EXISTS `$this->table_name` (" .
+			" `{$this->element_name}_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'value identifier', " .
+			' `parent_node_id` int(10) unsigned NOT NULL COMMENT \'parent node identifier\',' .
+			" `$this->element_name` varchar(256) COLLATE utf8_bin NOT NULL COMMENT 'value text'," .
+			" PRIMARY KEY (`{$this->element_name}_id`)," .
+			' UNIQUE KEY `parent_node_id` (`parent_node_id`)' .
+			') ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin' .
+			';';
+		$result = $this->database->execute($query);
+		
+		return $result;
+	}
+	
+	//------------------------------------------------------------------
+	// linking context storage (creating table relations)
+	//------------------------------------------------------------------
+	
+	function link_storage(){
+		$query =
+			"ALTER TABLE `$this->table_name`" .
+			" ADD CONSTRAINT `{$this->table_name}_ibfk_1`" .
+			' FOREIGN KEY (`parent_node_id`)' .
+			' REFERENCES `nodes` (`node_id`)' .
+			' ON DELETE CASCADE' .
+			';';
+		$result = $this->database->execute($query);
+		
+		return $result;
+	}
+	
+	//------------------------------------------------------------------
+	// setting context
+	//------------------------------------------------------------------
+	
+	function set($parent_node_id, $value = ''){
+		
+		$query =
+			"INSERT $this->table_name" .
+			' SET' .
+			"  parent_node_id = $parent_node_id," .
+			"  $this->element_name = '{$this->database->escape_string($value)}'" .
+			' ON DUPLICATE KEY UPDATE' .
+			"  $this->element_name = '{$this->database->escape_string($value)}'" .
+			';';
+		$result = $this->database->execute($query);
+		
+		if($result === false){
+			return false;
+		}
+		
+		$context_id = $this->database->get_last_insert_id();
+		
+		return $context_id;
+		
+	}
+	
+	//------------------------------------------------------------------
+	// updating context
+	//------------------------------------------------------------------
+	
+	function update($id, $value){
+		$query =
+			"UPDATE $this->table_name" .
+			" SET $this->element_name = '{$this->database->escape_string($value)}'" .
+			" WHERE {$this->element_name}_id = $id" .
+			';';
+		echo($query);
+		$result = $this->database->execute($query);
+		
+		if($result === false){
+			return false;
+		}
+		
+		$affected_rows = $this->database->get_affected_rows();
+		
+		return $affected_rows;
+	}
+	
+	//------------------------------------------------------------------
+	// deleting context
+	//------------------------------------------------------------------
+	
+	function delete($id){
+		
+		$query =
+			"DELETE FROM $this->table_name" .
+			" WHERE {$this->element_name}_id = $id" .
+			';';
+		$result = $this->database->execute($query);
+		
+		if($result === false){
+			return false;
+		}
+		
+		$affected_rows = $this->database->get_affected_rows();
+		
+		return $affected_rows;
+	}
+	
+}
--- data/mysql/context.php
+++ data/mysql/context.php
@@ -2,45 +2,12 @@
 
 namespace Dictionary;
 
-require_once __DIR__ . '/abstracts/mapper.php';
+require_once __DIR__ . '/abstracts/single_value.php';
 
-class MySQL_Context extends MySQL_Mapper {
+class MySQL_Context extends MySQL_Single_Value {
 	
-	//------------------------------------------------------------------
-	// creating context storage (table)
-	//------------------------------------------------------------------
-	
-	function create_storage(){
-		$query =
-			'CREATE TABLE IF NOT EXISTS `contexts` (' .
-			' `context_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT \'context identifier\', ' .
-			' `parent_node_id` int(10) unsigned NOT NULL,' .
-			' `context` varchar(256) COLLATE utf8_bin NOT NULL,' .
-			' PRIMARY KEY (`context_id`),' .
-			' UNIQUE KEY `parent_node_id` (`parent_node_id`)' .
-			') ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin' .
-			';';
-		$result = $this->database->execute($query);
-		
-		return $result;
-	}
-	
-	//------------------------------------------------------------------
-	// linking context storage (creating table relations)
-	//------------------------------------------------------------------
-	
-	function link_storage(){
-		$query =
-			'ALTER TABLE `contexts`' .
-			' ADD CONSTRAINT `contexts_ibfk_1`' .
-			' FOREIGN KEY (`parent_node_id`)' .
-			' REFERENCES `nodes` (`node_id`)' .
-			' ON DELETE CASCADE' .
-			';';
-		$result = $this->database->execute($query);
-		
-		return $result;
-	}
+	protected $table_name    = 'contexts';
+	protected $element_name  = 'context';
 	
 	//------------------------------------------------------------------
 	// hydrate a node with context 
@@ -61,51 +28,6 @@ class MySQL_Context extends MySQL_Mapper {
 			$context->set($context_result['context']);
 		}
 		
-	}
-	
-	//------------------------------------------------------------------
-	// setting context
-	//------------------------------------------------------------------
-	
-	function set($parent_node_id, $context){
-		
-		// inserting node to category label relation
-		
-		$query =
-			'INSERT contexts' .
-			' SET' .
-			"  parent_node_id = $parent_node_id," .
-			"  context = '{$this->database->escape_string($context)}'" .
-			' ON DUPLICATE KEY UPDATE' .
-			"  context = '{$this->database->escape_string($context)}'" .
-			';';
-		$result = $this->database->execute($query);
-		
-		if($result === false) return false;
-		
-		$affected_rows = $this->database->get_affected_rows();
-		
-		return $affected_rows;
-		
-	}
-	
-	//------------------------------------------------------------------
-	// deleting context
-	//------------------------------------------------------------------
-	
-	function delete($parent_node_id){
-		
-		$query =
-			'DELETE FROM contexts' .
-			" WHERE parent_node_id = $parent_node_id" .
-			';';
-		$result = $this->database->execute($query);
-		
-		if($result === false) return false;
-		
-		$affected_rows = $this->database->get_affected_rows();
-		
-		return $affected_rows;
 	}
 	
 }
